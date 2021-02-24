2d0b24edb4bab103f3b73cdee846613d7530e89a
Anyi Lorena <lorena0118a@gmail.com>
2021-02-23 22:48:19 -0500

    general services started

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+//Import database_conection
+require('./db_config/db')
+
+//importing routes
+const generalServices = require('./routes/general/general')
 
 //Settings
 app.set('port', process.env.PORT || 5300);
@@ -13,6 +18,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+
+app.use('/', generalServices)
 
 // 
 app.listen(app.get('port'), ()=>{
