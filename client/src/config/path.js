6fa282da538164d52692c716a1425bec2475658e
JuanEstebanCC <juanescifuentes75@gmail.com>
2021-03-06 23:13:16 -0500

    Security issues for the server with Helmet, good practices for performance, meta tag's on html, server prepared for build and serve the frontend, start the dashboard page

--- client/src/config/path.js
+++ client/src/config/path.js
@@ -1,5 +1,6 @@
-export const _home = '/';
-export const _login = '/login';
-export const _ChatPatient = '/ChatPatient';
-export const _ChatDoctor = '/ChatDoctor';
-export const _Register = '/Register';
+export const _home = "/";
+export const _login = "/login";
+export const _ChatPatient = "/ChatPatient";
+export const _ChatDoctor = "/ChatDoctor";
+export const _Register = "/Register";
+export const _Dashboard = "/Dashboard";
--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Switch} from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 //Import private rout
-import PrivateRoute from './components/privateRout';
+import PrivateRoute from "./components/privateRout";
 //Import public rout
-import PublicRoute from './components/publicRout';
+import PublicRoute from "./components/publicRout";
 //Import auth contenxt provider
-import AuthProvider from './contexts/authContext';
+import AuthProvider from "./contexts/authContext";
 
 //Import path
-import {_home, _login, _ChatPatient, _ChatDoctor, _Register} from './config/path';
+import {
+  _home,
+  _login,
+  _ChatPatient,
+  _ChatDoctor,
+  _Register,
+  _Dashboard,
+} from "./config/path";
 
 //Import pages
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/dashboard";
 import ChatPatient from "./pages/chatPatient";
 import ChatDoctor from "./pages/chatDoctor";
-import Register from "./pages/Register";
 
 const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
-      <Switch>
-        <PublicRoute exact path={_home} component={Home} />
-        <PublicRoute path={_login} component={Login} />
-        <PrivateRoute path={_ChatPatient} component={ChatPatient} />
-        <PrivateRoute path={_ChatDoctor} component={ChatDoctor} />
-        <PublicRoute path={_Register} component={Register} />
-      </Switch>
+        <Switch>
+          <PublicRoute exact path={_home} component={Home} />
+          <PublicRoute path={_login} component={Login} />
+          <PrivateRoute path={_ChatPatient} component={ChatPatient} />
+          <PrivateRoute path={_ChatDoctor} component={ChatDoctor} />
+          <PublicRoute path={_Dashboard} component={Dashboard} />
+          <PublicRoute path={_Register} component={Register} />
+        </Switch>
       </BrowserRouter>
     </AuthProvider>
-    
   );
 };
 
