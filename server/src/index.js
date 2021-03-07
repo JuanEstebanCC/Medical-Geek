3c7dd88e8cbf7242558799a6081a5bdc2aa0c8ff
JuanEstebanCC <juanescifuentes75@gmail.com>
2021-03-06 23:12:18 -0500

    Security issues for the server with Helmet, good practices for performance, meta tag's on html, server prepared for build and serve the frontend, start the dashboard page

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,27 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
+const compression = require("compression");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const cors = require("cors");
 const app = express();
-const socketio = require('socket.io');
-const http = require('http');
+const socketio = require("socket.io");
+const http = require("http");
 //Import database_conection
 require("./db_config/db");
+app.use(helmet());
 
 //importing routes
 const generalServices = require("./routes/general/general");
 //Settings
 app.set("port", process.env.PORT || 5300);
+
+// Build of the frontend
+app.use(express.static(path.join(__dirname, "../../client/build")));
+
+// Compresion settings
+app.use(compression());
 
 //middlewares
 app.use(morgan("dev"));
@@ -22,15 +32,18 @@ app.use(express.urlencoded({ extended: true }));
 //Set the endpoints
 app.use(require("./routes/send_mail/send_mail"));
 app.use("/", generalServices);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-app.use(generalServices)
+app.use(generalServices);
 
 //Socket.io
 const server = http.createServer(app);
 const io = socketio(server);
 //Import connection socket
-require('./sockets/sockets')(io);
+require("./sockets/sockets")(io);
 
-server.listen(app.get('port'), ()=>{
-    console.log(`Server running on port ${app.get('port')}`)
+server.listen(app.get("port"), () => {
+  console.log(`Server running on port ${app.get("port")}`);
 });
