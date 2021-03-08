4be95431456717f21b9b98451b42365d9e5860bb
SebasRpox <sebaxrpo@gmail.com>
2021-03-07 23:44:40 -0500

    Add cdn materialize and logo MedicalGeek

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,18 +5,18 @@ import "../styles/styles.css";
 const Home = () => {
   return (
     <body>
-      <div class="div-home">
-        <h1 class="home-title">Chat with a doctor</h1>
+      <div className="div-home">
+        <h1 className="home-title">Chat with a doctor</h1>
         <span className="img-home"></span>
         <p class="p-home">
           Worry no more about diet, medicine, or medical appointment All in one
           place.
         </p>
         <a class="button-home" href="/login">
-          <span class="text-button-home">Register</span>
+          <span className="text-button-home">Register</span>
         </a>
       </div>
-      <footer class="footer-home"></footer>
+      <footer className="footer-home"></footer>
     </body>
   );
 };
